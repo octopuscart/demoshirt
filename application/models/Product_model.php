54d89c0479cf6870927d1492cb3417f5c56ec85d
dev1 <dev@octopuscart.com>
2018-06-08 17:58:16 +0530

    change query style

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -17,14 +17,7 @@ class Product_model extends CI_Model {
 
     public function query_exe($query) {
         $query = $this->db->query($query);
-        if ($query->num_rows() > 0) {
-            foreach ($query->result_array() as $row) {
-                $data[] = $row;
-            }
-            return $data; //format the array into json data
-        } else {
-            return array();
-        }
+        return $query->result_array();
     }
 
     function delete_table_information($tableName, $columnName, $id) {
